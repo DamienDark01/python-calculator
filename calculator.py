ec69280d8250126de4b5d94b256809bd2bc2a46d
Damith Induranga <105861697+DamienDark01@users.noreply.github.com>
2023-03-31 19:54:19 +0000

    added comments

--- calculator.py
+++ calculator.py
@@ -1,18 +1,25 @@
+# custom function to add
 def add():
   
 
+# custom function to subtract
 def subtract():
   
 
+# custom function to multiply
 def multiply():
   
 
+# custom function to divide
 def divide():
   
 
+# custom function to find the power
 def power():
   
 
+# custom function to find the remainder
 def remainder():
 
 
+# rest of the program
